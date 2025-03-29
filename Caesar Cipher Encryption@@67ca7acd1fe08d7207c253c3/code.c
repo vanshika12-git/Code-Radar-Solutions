b1326154f65441ce57ed0a34958c4c5520ab5564
Vanshika <vanshika05126@gmail.com>
2025-03-29 11:39:22 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -5,11 +5,11 @@
 void CaesarCipher(char[], int);
 { 
     for (int i = 0; i < strlen(message); i++)
-     {
+     
         if (isalpha(message[i]))
          { 
             char base = islower(message[i]) ? 'a' : 'A'; 
             message[i] = (message[i] - base + shift) % 26 + base;
         }
-    }
+
 }
